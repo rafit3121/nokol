53dbad9b67b3b9d2e27b40eccbc6f1ee54da46a0
alihasna32 <muktermukter555@gmail.com>
2025-12-18 17:07:16 +0600

    Ready for deploy

--- src/pages/ComputerForth.jsx
+++ src/pages/ComputerForth.jsx
@@ -76,6 +76,21 @@ const ComputerThird = () => {
                             </a>
                         </div>
                     </div>
+                    <div className="card">
+                        <div className="img">
+                            <img src="/images/Web-Design-Development1.jpg" alt="Web Design Development Material" />
+                        </div>
+                        <div className="card-details">
+                            <h3>Web Design Development</h3>
+                            <a
+                                href="https://forms.gle/avoxnKjbqvLMyPDFA"
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                Download Now
+                            </a>
+                        </div>
+                    </div>
                 </section>
             </main>
         </div>
